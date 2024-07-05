71223ab32277767b15c18efee106fed39761b05e
João Barata <14177189+joaobarata@users.noreply.github.com>
2024-07-05 15:36:48 +0100

    Initial Version

---
+++ BinaryConcatenator.cs
@@ -0,0 +1,16 @@
+﻿using OutSystems.ExternalLibraries.SDK;
+
+namespace Outsystems.BinaryConcat
+{
+    public class BinaryConcatenator : IBinaryConcat
+    {
+        public byte[] BinaryConcat( byte[] BinaryField1, byte[] BinaryField2)
+        {
+            byte[] binaryResult = new byte[BinaryField1.Length + BinaryField2.Length];
+            Buffer.BlockCopy(BinaryField1, 0, binaryResult, 0, BinaryField1.Length);
+            Buffer.BlockCopy(BinaryField2, 0, binaryResult, BinaryField1.Length, BinaryField2.Length);
+
+            return binaryResult;
+        }
+    }
+}
---
+++ IBinaryConcat.cs
@@ -0,0 +1,15 @@
+﻿using OutSystems.ExternalLibraries.SDK;
+
+namespace Outsystems.BinaryConcat
+{
+    [OSInterface(Description = "This extension provides an action that allows to concatenate two Binary Data variables.", IconResourceName = "BinaryConcat.resources.logo.png", Name = "BinaryConcat")]
+    public interface IBinaryConcat
+    {
+        [OSAction(Description = "Concatenate two Binary Data variables.", IconResourceName = "BinaryConcat.resources.logo.png", ReturnName = "BinaryResult")]
+        public byte[] BinaryConcat(
+            [OSParameter(Description = "First Binary Data field.")]
+            byte[] BinaryField1,
+            [OSParameter(Description = "Second Binary Data field.")]
+            byte[] BinaryField2);
+    }
+}
