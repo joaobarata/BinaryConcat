6c182cdd1e895f426cd735c1de5328de615b73b8
João Barata <14177189+joaobarata@users.noreply.github.com>
2025-09-03 21:21:22 +0100

    Add checks to exit early on empty binaries

--- BinaryConcatenator.cs
+++ BinaryConcatenator.cs
@@ -6,6 +6,16 @@ namespace Outsystems.BinaryConcat
     {
         public byte[] BinaryConcat( byte[] BinaryField1, byte[] BinaryField2)
         {
+
+            if(BinaryField1 == null || BinaryField1.Length == 0)
+            {
+                return BinaryField2 ?? Array.Empty<byte>();
+            }
+            if (BinaryField2 == null || BinaryField2.Length == 0)
+            {
+                return BinaryField1;
+            }
+
             byte[] binaryResult = new byte[BinaryField1.Length + BinaryField2.Length];
             Buffer.BlockCopy(BinaryField1, 0, binaryResult, 0, BinaryField1.Length);
             Buffer.BlockCopy(BinaryField2, 0, binaryResult, BinaryField1.Length, BinaryField2.Length);
